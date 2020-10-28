65d8a1f034e155e6901be714699e31379e7cb77f
Frankley F. Rocha <rocha112358@gmail.com>
2020-10-28 12:41:58 -0300

    update run_example.sh

--- run_example.sh
+++ run_example.sh
@@ -2,7 +2,8 @@
 
 #cria o container
 docker run \
-    -p "80:80" \
+    -p "8080:80" \
+    -p "443:443" \
     -v ${PWD}/app:/app \
     --name php_pdo_example \
     -d \
@@ -16,4 +17,5 @@ docker exec php_pdo_example sh -c 'mysql -u root < /app/db.sql'
 
 #abrir a aplicação
 echo 'Tudo pronto!'
-echo 'Você pode acessar a apliacação em: http://localhost'
+echo 'Você pode acessar a apliacação em: http://localhost:8080'
+echo 'Você pode administrar o banco de dados em: http://localhost:8080/phpmyadmin'
