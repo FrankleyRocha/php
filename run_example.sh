dc85f192cb4ec87343cc203f59ee03ff3b569343
Frankley F. Rocha <rocha112358@gmail.com>
2020-10-28 13:10:50 -0300

    update run_example.sh

--- run_example.sh
+++ run_example.sh
@@ -3,19 +3,33 @@
 #cria o container
 docker run \
     -p "8080:80" \
-    -p "443:443" \
     -v ${PWD}/app:/app \
     --name php_pdo_example \
     -d \
     mattrayner/lamp:latest-1804
 
-#aguarda um minuto para inicializar os serviçes (apache, mysql e etç)
-sleep 30
+function create_php_pdo_example_db {
+    #criação do banco de dados
+    docker exec php_pdo_example \
+        sh -c 'mysql -u root < /app/db.sql' \
+            2 > /dev/null \
+            >> /dev/null
+}
 
-#criação do banco de dados
-docker exec php_pdo_example sh -c 'mysql -u root < /app/db.sql'
+echo 'aguarde um momento, inicializando.'
+sleep 1
+create_php_pdo_example_db
 
-#abrir a aplicação
-echo 'Tudo pronto!'
-echo 'Você pode acessar a apliacação em: http://localhost:8080'
-echo 'Você pode administrar o banco de dados em: http://localhost:8080/phpmyadmin'
+until [ $? -eq 0 ]; do
+    #aguarda um minuto para inicializar os serviçes (apache, mysql e etç)
+    sleep 1
+    #criação do banco de dados
+    create_php_pdo_example_db
+done
+
+if [ $? -eq 0 ]
+then
+    echo 'Tudo pronto!'
+    echo 'Você pode acessar a apliacação em: http://localhost:8080'
+    echo 'Você pode administrar o banco de dados em: http://localhost:8080/phpmyadmin'
+fi
